1df2e5dfe3b75b377d8ab0f04eb479e13f2b78b9
Dominik Robert <dominikrobert@gmx.de>
2023-03-21 17:20:57 +0100

    Neue Stunde

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 pebble_height = 3
-pebble_width = 2
+pebble_width = 1
 
 def in_range(value, start, end):
     if start <= value and value <= end:
@@ -12,24 +12,18 @@ def liegt_punkt_in_pebble(x, y, pos_x, pos_y):
         return True
     return False
 
+
 WIDTH = 40
 HEIGHT = 20
 
-ball_x = 10
+ball_x = 20
 ball_y = 10
 
-
-
-
 pebble_links_x = 1
-pebble_links_y = 1
+pebble_links_y = 10
 
 pebble_rechts_x = 38
-pebble_rechts_y = 1
-
-
-
-
+pebble_rechts_y = 10
 
 
 for y in range(0, HEIGHT):
@@ -37,12 +31,10 @@ for y in range(0, HEIGHT):
 
         if x == ball_x and y == ball_y:
             print("O", end="")
+        elif liegt_punkt_in_pebble(x, y, pebble_links_x, pebble_links_y):
+            print("|", end="")
+        elif liegt_punkt_in_pebble(x, y, pebble_rechts_x, pebble_rechts_y):
+            print("|", end="")
         else:
             print(" ", end="")
-
-        if liegt_punkt_in_pebble(x, y, pebble_links_x, pebble_links_y):
-            print("|", end="")
     print("")
-
-
-
